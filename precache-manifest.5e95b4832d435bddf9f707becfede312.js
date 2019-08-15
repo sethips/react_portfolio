cb11471b28cd4c365714b374a7bf1e3437691955
Luigi <virtual.employee@gmail.com>
2019-08-15 14:22:54 -0600

    Updates

---
+++ precache-manifest.5e95b4832d435bddf9f707becfede312.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6a4b27791185d6959a4971805df38a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2739c22b1c88e5cdc4ca",
+    "url": "/static/css/2.f208e258.chunk.css"
+  },
+  {
+    "revision": "5c85a2bf0325155f43ba",
+    "url": "/static/css/main.620a52b9.chunk.css"
+  },
+  {
+    "revision": "2739c22b1c88e5cdc4ca",
+    "url": "/static/js/2.8b2fc8ef.chunk.js"
+  },
+  {
+    "revision": "5c85a2bf0325155f43ba",
+    "url": "/static/js/main.d1c0c492.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9a1bd71c82838aa6d9f4f8df1c737ed4",
+    "url": "/static/media/Logo.9a1bd71c.svg"
+  },
+  {
+    "revision": "22ef9c0af4fba059463564ef46681db9",
+    "url": "/static/media/Me@2x.22ef9c0a.png"
+  },
+  {
+    "revision": "2a8411aed95747e5840aca64b066329c",
+    "url": "/static/media/logo_footer.2a8411ae.svg"
+  },
+  {
+    "revision": "456063febf95262743c59e33484c222c",
+    "url": "/static/media/name.456063fe.svg"
+  },
+  {
+    "revision": "a868e088f997ab5500ad1ccdac294b6a",
+    "url": "/static/media/nut.a868e088.svg"
+  }
+]);
